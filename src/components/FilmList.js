f7624a6f36a9a772a04c996814890884db332599
Alex Dodd <alexdodd@gmail.com>
2021-03-01 15:41:59 +0000

    added basic hello world text

---
+++ src/components/FilmList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Film from './Film.js';
+
+const FilmList = () => {
+	return (
+		<>
+			<Film></Film>
+			<Film></Film>
+			<Film></Film>
+			<Film></Film>
+		</>
+	);
+};
+
+export default FilmList;
